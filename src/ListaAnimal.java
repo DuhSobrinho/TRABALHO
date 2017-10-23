41ea9d454b99c81ad06ecf9b5be174b2f7620921
Guilherme Barbosa Leme <guibleme@gmail.com>
2017-10-23 20:20:06 -0200

    Listagem de animais criado

--- src/ListaAnimal.java
+++ src/ListaAnimal.java
@@ -1,3 +1,11 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +16,43 @@
  *
  * @author Rodolfo
  */
+
+
 public class ListaAnimal {
-    
+     public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+        File arquivo = new File("teste.txt");
+        File folder = new File("./Trabalho/" + "Animais/");
+        File[] listOfFolders = folder.listFiles();
+        
+        for (int i = 0; i < listOfFolders.length; i++) {
+            File auxFolder = new File("./Trabalho/Animais/"+listOfFolders[i].getName());
+            File[] auxFile = auxFolder.listFiles();
+            FileInputStream fis = new FileInputStream(auxFile[0]);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+
+            String linha;
+            int counter = 0;
+            String aux = "";
+            while ((linha = br.readLine()) != null) {
+                
+               switch (counter) {
+                   case 0:
+                       aux+=(linha + ": ");
+                       break;
+                   case 1:
+                       aux+=(linha + " - ");
+                       break;
+                   case 3:
+                        aux+=linha;
+                       break;
+               }
+               counter++;
+            }
+            System.out.println(aux);
+            br.close();
+           
+        }
+    }
 }
