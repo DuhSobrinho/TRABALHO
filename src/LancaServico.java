3d69b358559ad4c9616fc2fd2c4d05c2fdba03f1
RodolfoBecker <rodolfofbecker@hotmail.com>
2017-10-25 20:37:19 -0200

    lanca servico terminada

--- src/LancaServico.java
+++ src/LancaServico.java
@@ -1,11 +1,7 @@
 
-<<<<<<< HEAD
-import java.io.File;
-=======
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
->>>>>>> master
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -21,41 +17,23 @@ public class LancaServico {
 
     static void executaLancaServico() {
         int quantidadeServicos = 0;
+        int opcao = 0;
         do {
             try {
 
                 String nomepet = pedeNome();
-<<<<<<< HEAD
                 File arqServico = GeraArquivoServico(nomepet);
                 if (arqServico.exists()) {
                     mostraOpcao(arqServico);
-
-=======
-                File servicoCriado = GeraArquivoServico(nomepet);
-
-                if (servicoCriado.exists()) {
-                    mostraOpcao(opcao);
-                } else {
-                    break;
->>>>>>> master
                 }
 
             } catch (Exception ex) {
                 Dialogo.mostraErro("Error", ex.getMessage());
-<<<<<<< HEAD
                 Menu.executaMenu();
 
             }
 
         } while (quantidadeServicos > 20);
-=======
-                opcao += -1;
-            }
-
-        } while (opcao == 0);
-        System.exit(0);
-
->>>>>>> master
     }
 
     static String pedeNome() {
@@ -70,11 +48,7 @@ public class LancaServico {
                 Menu.executaMenu();
             }
 
-<<<<<<< HEAD
         } while (nomePet.length() == 0);
-=======
-        } while (nomePet.length() != 0);
->>>>>>> master
 
         return nomePet;
     }
@@ -85,20 +59,6 @@ public class LancaServico {
         File arqServico = new File("Animais//" + nomepet, "servico.txt");
         if (arqValida.exists()) {
             arqServico.createNewFile();
-<<<<<<< HEAD
-=======
-            FileOutputStream fos = new FileOutputStream(arqServico);
-            OutputStreamWriter osw = new OutputStreamWriter(fos);
-            PrintWriter pw = new PrintWriter(osw);
-
-            //So falta alterar os testes p/ os servicos lancados!!!
-            pw.println("teste");
-            pw.println("teste");
-            pw.println("teste");
-            pw.println("teste");
-            pw.println("teste");
-            pw.close();
->>>>>>> master
 
         } else {
             Dialogo.mostraErro("Pasta Não Encontrada", "pasta: " + nomepet + "Não foi cadastrada,\nPor favor realizar cadastro!");
@@ -109,53 +69,51 @@ public class LancaServico {
 
     }
 
-    static String pedeServico(int contador, File arqServico) throws FileNotFoundException {
+    static String executa(File arqServico) throws FileNotFoundException, IOException {
         String servico = Dialogo.lerTextoObrigatorio("Lança Serviço", "Informe o serviço prestado: ");
-
-<<<<<<< HEAD
         String valorServico = Dialogo.lerTextoObrigatorio("Lança Serviço", "Informe o valor do serviço: ");
-
-        String servicoPrestado = servico + " - " + valorServico;
+        String ServicoCompleto = new String(servico + "," + valorServico);
+        
+        FileInputStream fis = new FileInputStream(arqServico);
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+        
+        String linha;
+        String auxInfo = "";
+        String auxOut = "";
+        int counter = 0;
+        while ((linha = br.readLine()) != null) {
+            
+            if(counter != 0) {
+                auxInfo += (linha + "\r\n");
+            }
+            counter++;
+        }
+        auxInfo += ServicoCompleto;
+        auxOut = Integer.toString(counter) + "\r\n" + auxInfo;
+        
+        
         FileOutputStream fos = new FileOutputStream(arqServico);
         OutputStreamWriter osw = new OutputStreamWriter(fos);
         PrintWriter pw = new PrintWriter(osw);
-        pw.println(servicoPrestado + "\n");
+
+        //So falta alterar os testes p/ os servicos lancados!!!
+        pw.println(auxOut);
         pw.close();
-        
-        System.out.println(contador);
-        return servico;
-        
+
+        return null;
     }
 
-    static int mostraOpcao(File arqServico) throws FileNotFoundException {
+    static int mostraOpcao(File arqServico) throws FileNotFoundException, IOException {
         int opcao = 0;
-        int contador = 0;
-=======
-    static int pedeValor() {
-        String valorServico = Dialogo.lerTextoObrigatorio("Lança Serviço", "Informe o valor do serviço: ");
-        return Integer.parseInt(valorServico);
-    }
-
-    static void executa() {
-        pedeServico();
-        pedeValor();
-    }
-
-    static int mostraOpcao(int opcao) {
->>>>>>> master
         opcao = Dialogo.lerInteiro("Ordem Serviço",
                 "1 - Adicionar serviço\n"
                 + "2 - Fechar ordem de serviços");
 
         switch (opcao) {
             case 1:
-<<<<<<< HEAD
-                contador++;
-                pedeServico(contador,arqServico);
+                executa(arqServico);
                 mostraOpcao(arqServico);
-=======
-                executa();
->>>>>>> master
                 break;
             case 2:
                 Menu.executaMenu();
@@ -163,47 +121,7 @@ public class LancaServico {
             default:
                 throw new AssertionError();
         }
-
-        return contador;
+        return opcao;
     }
 
-<<<<<<< HEAD
-=======
-    static String executaListaAnimal() throws FileNotFoundException, IOException {
-        File folder = new File("./" + "Animais/");
-        File[] listOfFolders = folder.listFiles();
-        String gambiarra = "";
-
-        for (int i = 0; i < listOfFolders.length; i++) {
-            File auxFolder = new File("./Animais/" + listOfFolders[i].getName());
-            File[] auxFile = auxFolder.listFiles();
-            FileInputStream fis = new FileInputStream(auxFile[0]);
-            InputStreamReader isr = new InputStreamReader(fis);
-            BufferedReader br = new BufferedReader(isr);
-
-            String linha;
-            int counter = 0;
-            String aux = "";
-            while ((linha = br.readLine()) != null) {
-
-                switch (counter) {
-                    case 0:
-                        aux += (linha + ": ");
-                        break;
-                    case 1:
-                        aux += (linha + " - ");
-                        break;
-                    case 3:
-                        aux += linha;
-                        break;
-                }
-                counter++;
-            }
-            System.out.println(aux);
-            gambiarra += aux + "\n";
-            br.close();
-        }
-        return gambiarra;
-    }
->>>>>>> master
 }
