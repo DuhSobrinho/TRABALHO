6e07cdcf4cd648113649e96e49e3c51698d239b2
Guilherme Barbosa Leme <guibleme@gmail.com>
2017-10-29 16:11:54 -0200

    Finalizado Detalhes de animal

--- src/Detalhes.java
+++ src/Detalhes.java
@@ -1,13 +1,102 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-/**
- *
- * @author Rodolfo
- */
 public class Detalhes {
+    public static void main(String[] args) throws Exception {
+       String nomepet = detalhesServico();
+       File arquivoServico = encontraArquivoServico(nomepet);
+       File arquivoDados = encontraArquivoDados(nomepet);
+       geraInvoice(arquivoServico, arquivoDados);
+    }
     
+    static String detalhesServico() {
+        String nomepet = LancaServico.pedeNome();
+        return nomepet;
+    }
+    
+    static File encontraArquivoServico(String nomepet) throws Exception {
+        File arqValida = new File("Animais", nomepet);
+        File arqServico = new File("Animais//" + nomepet, "servico.txt");
+        if (arqValida.exists()) {
+//            System.out.println("arqServico");
+            return arqServico;
+        } else {
+            Dialogo.mostraErro("Pasta Não Encontrada", "pasta: " + nomepet + "Não foi cadastrada,\nPor favor realizar cadastro!");
+            return null;
+        }
+    }
+    
+    static File encontraArquivoDados(String nomepet) throws Exception {
+        File arqValida = new File("Animais", nomepet);
+        File arqServico = new File("Animais//" + nomepet, "dados.txt");
+        if (arqValida.exists()) {
+//            System.out.println("arqdados");
+            return arqServico;
+        } else {
+            Dialogo.mostraErro("Pasta Não Encontrada", "pasta: " + nomepet + "Não foi cadastrada,\nPor favor realizar cadastro!");
+            return null;
+        }
+    }
+    
+    static String geraInvoice(File arquivoServico, File arquivoDados) throws FileNotFoundException, IOException {
+        String linha = "";
+        String auxLinha = "";
+        int auxValue = 0;
+        int counter = 0;
+        
+        File arquivoDado = arquivoDados;
+        FileInputStream fis = new FileInputStream(arquivoDado);
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br;
+        br = new BufferedReader(isr);
+        
+        while ((linha = br.readLine()) != null) {
+            switch (counter) {
+                case 0:
+                    auxLinha += ("Nome: " + linha + "\r\n");
+                    break;
+                case 1:
+                    auxLinha += ("Espécie: " + linha + "\r\n");
+                    break;
+                case 2:
+                    auxLinha += ("Raça: " + linha + "\r\n");
+                    break;
+                case 3:
+                    auxLinha += ("Dono: " + linha + "\r\n");
+                    break;
+                case 4:
+                    auxLinha += ("Nascimento: " + linha + "\r\n");
+                    break;
+                default:
+                    break;
+            }
+              counter++;
+           }
+        auxLinha += ("\r\n");
+        counter = 0;
+        File arquivoServicos = arquivoServico;
+        FileInputStream fis2 = new FileInputStream(arquivoServicos);
+        InputStreamReader isr2 = new InputStreamReader(fis2);
+        BufferedReader br2;
+        br2 = new BufferedReader(isr2);
+        
+        while ((linha = br2.readLine()) != null) {
+              if(counter != 0){
+                  String kept = linha.substring( 0, linha.indexOf(","));
+                  String remainder = linha.substring(linha.indexOf(",")+1, linha.length());
+                  auxValue += Integer.parseInt(remainder);
+//                  System.out.println(auxValue);
+                  auxLinha += linha + "\r\n";
+              }
+              
+              counter++;
+           }
+        auxLinha += ("Total: " + Integer.toString(auxValue) + "\r\n");
+        System.out.println(auxLinha);
+        return auxLinha;
+    }
 }
--- src/LancaServico.java
+++ src/LancaServico.java
@@ -1,58 +1,123 @@
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+ 
 public class LancaServico {
-
+ 
     public static void main(String[] args) {
+        executaLancaServico();
+    }
+ 
+    static void executaLancaServico() {
         int quantidadeServicos = 0;
         int opcao = 0;
         do {
             try {
-                
-
+ 
+                String nomepet = pedeNome();
+                File arqServico = GeraArquivoServico(nomepet);
+                if (arqServico.exists()) {
+                    mostraOpcao(arqServico);
+                }
+ 
             } catch (Exception ex) {
                 Dialogo.mostraErro("Error", ex.getMessage());
+                Menu.executaMenu();
+ 
             }
-
-        } while (quantidadeServicos == 0);
+ 
+        } while (quantidadeServicos > 20);
     }
-
+ 
     static String pedeNome() {
-        String nomePet = Dialogo.lerTextoObrigatorio("Lança Serviço", "Informe o nome do pet: ");
+        String nomePet;
+        do {
+             nomePet = null;
+            try {
+                nomePet = Dialogo.lerTextoObrigatorio("Lança Serviço", "Informe o nome do pet: ");
+ 
+            } catch (Exception ex) {
+                Dialogo.mostraErro("Error", ex.getMessage());
+                Menu.executaMenu();
+            }
+ 
+        } while (nomePet.length() == 0);
         return nomePet;
     }
-
-    static String pedeServico() {
-        String serviço = Dialogo.lerTextoObrigatorio("Lança Serviço", "Informe o serviço prestado: ");
-        return serviço;
+    
+    static File GeraArquivoServico(String nomepet) throws Exception {
+ 
+        File arqValida = new File("Animais", nomepet);
+        File arqServico = new File("Animais//" + nomepet, "servico.txt");
+        if (arqValida.exists()) {
+            arqServico.createNewFile();
+ 
+        } else {
+            Dialogo.mostraErro("Pasta Não Encontrada", "pasta: " + nomepet + "Não foi cadastrada,\nPor favor realizar cadastro!");
+            return null;
+        }
+ 
+        return arqServico;
+ 
     }
-
-    static int pedeValor(int quantidadeServicos) {
+    
+    static String executa(File arqServico) throws FileNotFoundException, IOException {
+        String servico = Dialogo.lerTextoObrigatorio("Lança Serviço", "Informe o serviço prestado: ");
         String valorServico = Dialogo.lerTextoObrigatorio("Lança Serviço", "Informe o valor do serviço: ");
-        quantidadeServicos++;
-        return Integer.parseInt(valorServico);
-    }
-
-    static void executa(int quantidadeServiços) {
-        pedeServico();
-        pedeValor(quantidadeServiços);
-    }
-    static int mostraOpcao(int opcao, int quantidadeServiços){
-        opcao = Dialogo.lerInteiro("Ordem Serviço", 
-                "1 - Adicionar serviço" 
-                + "2 - Ferchar ordem de serviços");
+        String ServicoCompleto = new String(servico + "," + valorServico);
         
+        FileInputStream fis = new FileInputStream(arqServico);
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+        
+        String linha;
+        String auxInfo = "";
+        String auxOut = "";
+        int counter = 0;
+        while ((linha = br.readLine()) != null) {
+            
+            if(counter != 0) {
+                auxInfo += (linha + "\r\n");
+            }
+            counter++;
+        }
+        auxInfo += ServicoCompleto;
+        auxOut = Integer.toString(counter) + "\r\n" + auxInfo;
+        
+        
+        FileOutputStream fos = new FileOutputStream(arqServico);
+        OutputStreamWriter osw = new OutputStreamWriter(fos);
+        PrintWriter pw = new PrintWriter(osw);
+ 
+        //So falta alterar os testes p/ os servicos lancados!!!
+        pw.println(auxOut);
+        pw.close();
+ 
+        return null;
+    }
+    static int mostraOpcao(File arqServico) throws FileNotFoundException, IOException {
+        int opcao = 0;
+        opcao = Dialogo.lerInteiro("Ordem Serviço",
+                "1 - Adicionar serviço\n"
+                + "2 - Fechar ordem de serviços");
+ 
         switch (opcao) {
-                    case 0:
-                        pedeNome();
-                        executa(quantidadeServiços);
-                        break;
-                    case 1:
-                        executa(quantidadeServiços); //inserir novo serviço
-                        break;
-                    case 2:
-                        //
-                    default:
-                        throw new AssertionError();
-                }
+            case 1:
+                executa(arqServico);
+                mostraOpcao(arqServico);
+                break;
+            case 2:
+                Menu.executaMenu();
+                break;
+            default:
+                throw new AssertionError();
+        }
         return opcao;
     }
 }
